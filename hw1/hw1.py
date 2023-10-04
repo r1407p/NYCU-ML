af5d1f824a4fd7c9ae7c18c3a06cb2e34cc00a0c
r1407p <buszter0403@gmail.com>
2023-10-05 02:38:59 +0800

    hw1 v1

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -9,32 +9,87 @@ class LinearRegression:
         self.closed_form_intercept = None
         self.gradient_descent_weights = None
         self.gradient_descent_intercept = None
+        self.train_loss = []
         
     # This function computes the closed-form solution of linear regression.
     def closed_form_fit(self, X, y):
         # Compute closed-form solution.
         # Save the weights and intercept to self.closed_form_weights and self.closed_form_intercept
+        
+        X_with_ones = np.insert(X, 0, 1, axis=1)
+        # print(X_with_ones)
+        X_transpose = np.transpose(X_with_ones)
+        X_transpose_X = np.dot(X_transpose, X_with_ones)
+        X_transpose_X_inv = np.linalg.inv(X_transpose_X)
+        theta = np.dot(np.dot(X_transpose_X_inv, X_transpose), y)
+        
+        self.closed_form_weights = theta[1:]
+        self.closed_form_intercept = theta[0]
+        return
         pass
 
     # This function computes the gradient descent solution of linear regression.
-    def gradient_descent_fit(self, X, y, lr, epochs):
+    def gradient_descent_fit(self, X, y, lr, epochs, batch_size=32):
         # Compute the solution by gradient descent.
         # Save the weights and intercept to self.gradient_descent_weights and self.gradient_descent_intercept
+        sample_num, var_num = X.shape
+        
+        self.gradient_descent_weights = np.zeros(var_num)
+        self.gradient_descent_intercept = 0
+        self.train_loss = []
+        
+        for epoch in range(epochs):
+            #every epoch we shuffle the data to avoid the order of data
+            indices = np.arange(sample_num)
+            np.random.shuffle(indices)
+            # print(indices)
+            X = X[indices]
+            y = y[indices]
+            # print(X)
+            # print(y)
+            for batch in range(sample_num//batch_size):
+                # here is the start and end index of the batch
+                start = batch * batch_size
+                end = start + batch_size
+                # print(start, end)
+                X_batch = X[start:end]
+                y_batch = y[start:end]
+                
+                prediction = self.gradient_descent_predict(X_batch)
+                gradient_weight = (-2/batch_size) * np.dot(np.transpose(X_batch), (y_batch - prediction))
+                gradient_intercept = (-2/batch_size) * np.sum(y_batch - prediction)
+                self.gradient_descent_weights -= lr * gradient_weight
+                self.gradient_descent_intercept -= lr * gradient_intercept
+            
+            loss = self.gradient_descent_evaluate(X, y)
+            self.train_loss.append(loss)
+            # input()
+        return 
         pass
         
 
     # This function compute the MSE loss value between your prediction and ground truth.
     def get_mse_loss(self, prediction, ground_truth):
+        diff = prediction - ground_truth
+        diff = np.square(diff)
+        result = np.sum(diff) / len(diff)
+        return result
         # Return the value.
         pass
 
     # This function takes the input data X and predicts the y values according to your closed-form solution.
     def closed_form_predict(self, X):
+        result = np.dot(X, self.closed_form_weights)
+        result = result + self.closed_form_intercept
+        return result
         # Return the prediction.
         pass
 
     # This function takes the input data X and predicts the y values according to your gradient descent solution.
     def gradient_descent_predict(self, X):
+        result = np.dot(X, self.gradient_descent_weights)
+        result = result + self.gradient_descent_intercept
+        return result
         # Return the prediction.
         pass
     
@@ -53,6 +108,13 @@ class LinearRegression:
     # This function use matplotlib to plot and show the learning curve (x-axis: epoch, y-axis: training loss) of your gradient descent solution.
     # You don't need to call this function in your submission, but you have to provide the screenshot of the plot in the report.
     def plot_learning_curve(self):
+        plt.plot(range(1, len(self.train_loss) + 1), self.train_loss, marker='o', linestyle='-')
+        plt.title('Learning Curve')
+        plt.xlabel('Epoch')
+        plt.ylabel('Training Loss')
+        plt.grid(True)
+        plt.show()
+        return 
         pass
 
 # Do not modify the main function architecture.
@@ -64,6 +126,8 @@ if __name__ == "__main__":
     train_y = train_df["Performance Index"]
     train_x = train_x.to_numpy()
     train_y = train_y.to_numpy()
+    # print(train_x)
+    # print(train_y)
     
     # Model Training and Evaluation
     LR = LinearRegression()
@@ -72,7 +136,7 @@ if __name__ == "__main__":
     print("Closed-form Solution")
     print(f"Weights: {LR.closed_form_weights}, Intercept: {LR.closed_form_intercept}")
 
-    LR.gradient_descent_fit(train_x, train_y, lr=0.01, epochs=100)
+    LR.gradient_descent_fit(train_x, train_y, lr=0.000180, epochs=1500)
     print("Gradient Descent Solution")
     print(f"Weights: {LR.gradient_descent_weights}, Intercept: {LR.gradient_descent_intercept}")
 
@@ -82,6 +146,12 @@ if __name__ == "__main__":
     test_x = test_x.to_numpy()
     test_y = test_y.to_numpy()
 
+    # closed_form_loss = LR.closed_form_evaluate(train_x, train_y)
+    # gradient_descent_loss = LR.gradient_descent_evaluate(train_x, train_y)
+    # print(f"Error Rate: {((gradient_descent_loss - closed_form_loss) / closed_form_loss * 100):.1f}%")
+
     closed_form_loss = LR.closed_form_evaluate(test_x, test_y)
     gradient_descent_loss = LR.gradient_descent_evaluate(test_x, test_y)
+    # print(closed_form_loss, gradient_descent_loss)
     print(f"Error Rate: {((gradient_descent_loss - closed_form_loss) / closed_form_loss * 100):.1f}%")
+    LR.plot_learning_curve()
